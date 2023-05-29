83734d1cc43db1836592bda6c0ec8518b9c701ad
Ilyes Mechouar <ilyes.mechouar@epitech.eu>
2023-05-29 16:24:43 +0200

    feat: first step

--- sources/main.c
+++ sources/main.c
@@ -5,7 +5,14 @@
 ** memory
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[])
 {
+    char *str = malloc(sizeof("Hello world!"));
+    str = argv[1];
+    printf("%s\n", str);
+    free(str);
     return 0;
 }
