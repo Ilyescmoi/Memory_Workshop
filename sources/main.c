4a057c84a1956a4af29e1001ddec65bbb7447684
Ilyes Mechouar <ilyes.mechouar@epitech.eu>
2023-05-29 17:09:07 +0200

    feat: my_malloc

--- sources/main.c
+++ sources/main.c
@@ -7,12 +7,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+void *my_malloc(size_t size)
+{
+    void *check_end_data = sbrk(0);
+    void *add_data = sbrk(size);
+
+    if (check_end_data == (void*)-1) {
+        return NULL;
+    }
+    return add_data;
+}
 
 int main(int argc, char *argv[])
 {
     char *str = malloc(sizeof("Hello world!"));
-    str = argv[1];
-    printf("%s\n", str);
-    free(str);
+    size_t size = 10;
+    void *malloc = my_malloc(size);
+
+    if (malloc == NULL) {
+        printf("erreur memoire\n");
+    } else {
+        printf("mémoire alloué %p\n", malloc);
+    }
+
     return 0;
 }
